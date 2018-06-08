3ef7155cc019a6db8834589accb434ee60624886
Web講師_昼 <tahara@studiom-web.net>
2018-06-08 09:53:12 +0900

    add new file

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./css/app.css";
+
+export default class App extends React.Component{
+	render(){
+		return(
+			<div id="main">
+			<h1 className="title">{"Hello World!"}</h1>
+			</div>
+			)
+	}
+}
+ReactDOM.render(
+<App/>,
+document.getElementById('app')
+)
