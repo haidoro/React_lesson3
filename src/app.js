ebdd3a5f08e65f3113bf828f429725820aad2170
Tahara <tahara@studiom-web.net>
2018-06-13 21:21:14 +0900

    時間を追加
    	modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -7,9 +7,24 @@ class App extends React.Component
 		super()
 		this.state = {
 			input : "",
-			todos:[]
+			todos:[],
+			time: new Date(),
+			intervalId:""
 		}
 		this.addToDo = this.addToDo.bind(this)
+		this.tick = this.tick.bind(this)
+	}
+	tick(){
+		let { time } = this.state
+		time.setSeconds(time.getSeconds() + 1)
+		this.setState({time: time})
+	}
+	componentDidMount(){
+		let id = setInterval(this.tick, 1000)
+		this.setState({intervalId: id})
+	}
+	componentWillUnmount(){
+		clearInterval(this.state.intervalId)
 	}
 	addToDo(){
 		let { todos,input } = this.state
@@ -23,6 +38,8 @@ class App extends React.Component
 				<h1>新しいタスク</h1>
 				<input type="text" onChange={e => this.setState({input: e.target.value})} value={this.state.input} />
 				<button onClick={this.addToDo}>click</button>
+				<h2>現在の時間</h2>
+				<span>今の時間{this.state.time.toLocaleTimeString()}</span>
 				<h2>ToDo:</h2>
 				<ul>
 				{this.state.todos.map((todo)=>{
